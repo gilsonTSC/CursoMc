ad6274bd4805cb5fc0084ce8cf218044b3a39699
Gilson <gilson.costa@dce.ufpb.br>
2020-02-25 18:44:46 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/gilson/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.gilson.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gilson.cursomc.domain.Pedido;
+import com.gilson.cursomc.repositories.PedidoRepository;
+import com.gilson.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
