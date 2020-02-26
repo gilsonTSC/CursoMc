eb76e6387ba03cb6ecd4fdc15b2460db466ca6a4
Gilson <gilson.costa@dce.ufpb.br>
2020-02-26 09:14:36 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/gilson/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/gilson/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.gilson.cursomc.services.exceptions.DateIntegrityException;
 import com.gilson.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -16,5 +17,11 @@ public class ResourceExceptionHandler {
 	private ResponseEntity<StandarError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
 		StandarError err = new StandarError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DateIntegrityException.class)
+	private ResponseEntity<StandarError> DateIntegrity(DateIntegrityException e, HttpServletRequest request){
+		StandarError err = new StandarError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
--- src/main/java/com/gilson/cursomc/services/CategoriaService.java
+++ src/main/java/com/gilson/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.gilson.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.gilson.cursomc.domain.Categoria;
 import com.gilson.cursomc.repositories.CategoriaRepository;
+import com.gilson.cursomc.services.exceptions.DateIntegrityException;
 import com.gilson.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -14,20 +16,29 @@ public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DateIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+		}
 	}
 }
