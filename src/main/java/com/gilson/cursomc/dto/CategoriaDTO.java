a9e37d48a979a77d44409f64284bd95822eec9fd
Gilson <gilson.costa@dce.ufpb.br>
2020-02-26 09:28:51 -0300

    Listando todas as categorias com DTO

---
+++ src/main/java/com/gilson/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,36 @@
+package com.gilson.cursomc.dto;
+
+import java.io.Serializable;
+
+import com.gilson.cursomc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String name;
+	
+	public CategoriaDTO() {}
+
+	public CategoriaDTO(Categoria obj) {
+		this.id = obj.getId();
+		this.name = obj.getName();
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
