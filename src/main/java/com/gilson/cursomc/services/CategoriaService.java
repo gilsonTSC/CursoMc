108ac35047f5f9bc52316a26c6cbc5dfcc99026b
Gilson <gilson.costa@dce.ufpb.br>
2020-02-27 20:47:19 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/gilson/cursomc/services/CategoriaService.java
+++ src/main/java/com/gilson/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.gilson.cursomc.domain.Categoria;
+import com.gilson.cursomc.dto.CategoriaDTO;
 import com.gilson.cursomc.repositories.CategoriaRepository;
 import com.gilson.cursomc.services.exceptions.DateIntegrityException;
 import com.gilson.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getName());
 	}
 }
