b6726988db9e9ce689bbb810ad7eb972f79bdfed
Gilson <gilson.costa@dce.ufpb.br>
2020-02-29 23:11:59 -0300

    Validação customizada: email não repetido na atualização do cliente

--- src/main/java/com/gilson/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/gilson/cursomc/repositories/ClienteRepository.java
@@ -1,10 +1,9 @@
 package com.gilson.cursomc.repositories;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import org.springframework.transaction.annotation.Transactional;
+
 import com.gilson.cursomc.domain.Cliente;
 
 @Repository
