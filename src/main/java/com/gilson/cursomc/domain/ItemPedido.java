a962719a196dc83fbb991c898fadfc367c162223
Gilson <gilson.costa@dce.ufpb.br>
2020-03-04 20:39:33 -0300

    Subtotal e total do pedido

--- src/main/java/com/gilson/cursomc/domain/ItemPedido.java
+++ src/main/java/com/gilson/cursomc/domain/ItemPedido.java
@@ -31,6 +31,10 @@ public class ItemPedido implements Serializable{
 		this.id.setProduto(produto);
 	}
 	
+	public double getSubTotal() {
+		return (this.preco - this.deconto) * this.quantidade;
+	}
+	
 	@JsonIgnore
 	public Pedido getPedido() {
 		return this.id.getPedido();
