44d05bb351450b03cf26a4a2811cc319c383e4ce
Gilson <gilson.costa@dce.ufpb.br>
2020-03-08 14:45:08 -0300

    Implementando toString de Pedido

--- src/main/java/com/gilson/cursomc/domain/ItemPedido.java
+++ src/main/java/com/gilson/cursomc/domain/ItemPedido.java
@@ -1,6 +1,8 @@
 package com.gilson.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
@@ -107,5 +109,20 @@ public class ItemPedido implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		StringBuilder builder = new StringBuilder();
+		builder.append(this.getProduto().getNome());
+		builder.append(", Qte: ");
+		builder.append(this.getQuantidade());
+		builder.append(", Preço unitário: ");
+		builder.append(nf.format(this.getPreco()));
+		builder.append(", Subtotal: ");
+		builder.append(nf.format(this.getSubTotal()));
+		builder.append("\n");
+		return builder.toString();
 	}
 }
