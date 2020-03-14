6ab53406c84e0e5458afed4e1614fd805dbb761f
Gilson <gilson.costa@dce.ufpb.br>
2020-03-14 09:50:55 -0300

    Refresh token

--- src/main/java/com/gilson/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/gilson/cursomc/services/AbstractEmailService.java
@@ -36,7 +36,7 @@ public abstract class AbstractEmailService implements EmailService {
 		SimpleMailMessage sm = new SimpleMailMessage();
 		sm.setTo(obj.getCliente().getEmail());
 		sm.setFrom(sender);
-		sm.setSubject("Peido confirmado! Código: " + obj.getId());
+		sm.setSubject("Pedido confirmado! Código: " + obj.getId());
 		sm.setSentDate(new Date(System.currentTimeMillis()));
 		sm.setText(obj.toString());
 		return sm;
