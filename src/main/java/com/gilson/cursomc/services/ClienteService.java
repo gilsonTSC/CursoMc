73d68fe9df2572b71344d4466c311d03436a1dd0
Gilson <gilson.costa@dce.ufpb.br>
2020-03-18 21:07:36 -0300

    Enviando imagem via endpoint

--- src/main/java/com/gilson/cursomc/services/ClienteService.java
+++ src/main/java/com/gilson/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.gilson.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.gilson.cursomc.domain.Cidade;
 import com.gilson.cursomc.domain.Cliente;
@@ -35,6 +37,9 @@ public class ClienteService {
 	
 	@Autowired
 	private BCryptPasswordEncoder pe;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	//@Autowired
 	//private EnderecoRepository enderecoRepository;
@@ -104,5 +109,9 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 }
