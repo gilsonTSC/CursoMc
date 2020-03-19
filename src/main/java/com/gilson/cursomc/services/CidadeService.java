85a83d4c4eea19530430890624fcd81e5a92fe0d
Gilson <gilson.costa@dce.ufpb.br>
2020-03-19 19:46:13 -0300

    Endpoints para buscar estados e cidades

---
+++ src/main/java/com/gilson/cursomc/services/CidadeService.java
@@ -0,0 +1,20 @@
+package com.gilson.cursomc.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gilson.cursomc.domain.Cidade;
+import com.gilson.cursomc.repositories.CidadeRepository;
+
+@Service
+public class CidadeService {
+
+	@Autowired
+	private CidadeRepository repo;
+	
+	public List<Cidade> findByEstado(Integer estadoId){
+		return repo.findCidades(estadoId);
+	}
+}
